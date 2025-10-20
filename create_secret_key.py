661b346e7beb3981e0ce2a3f07f85f695a016d3d
Leslie <vampruara@gmail.com>
2025-10-20 15:30:04 -0400

    File for SECRET_KEY generation
    Fixed typo preventing code to work and generate secret key into .env

--- create_secret_key.py
+++ create_secret_key.py
@@ -4,7 +4,7 @@ import secrets
 env_path = ".env"
 #loads .env
 lines = []
-if os.pathexists(env_path):
+if os.path.exists(env_path):
     with open(env_path, "r") as f:
         lines = f.readlines()
 
@@ -18,5 +18,6 @@ else:
     
     with open(env_path, "w") as f:
         f.writelines(lines)
+
 
     print(f"SECRET_KEY has been generated and added to {env_path}: {secret_key}")
